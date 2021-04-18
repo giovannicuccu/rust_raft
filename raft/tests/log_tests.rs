1d12eecb35c10ee99a0ee57928ff1b22768c4fbb
Giovanni Cuccu <giovanni.cuccu@gmail.com>
2021-04-18 17:02:41 +0200

    refactor test

--- raft/tests/log_tests.rs
+++ raft/tests/log_tests.rs
@@ -5,31 +5,53 @@ use raft::log::{WriteAheadLog, RecordEntryIterator};
 use std::convert::TryInto;
 use std::{env, fs};
 
-#[test]
-fn testWriteAndReadOneRecord() {
+fn createVectorDataforTest(len: u32) -> Vec<u8> {
+    let mut data: Vec<u8>=Vec::new();
+    for i in 0..len {
+        data.push(0);
+    }
+    data[0]=1;
+    data[1]=2;
+    data[2]=3;
+    data[3]=4;
+    data
+}
 
+fn testReadAndWriteSingleBlock(inputDataLen: u32, expected_wal_blocks: u16) {
     let mut dir = env::temp_dir();
     let mut rng = rand::thread_rng();
     let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
     println!("dir={}",dir);
     create_dir(&dir).unwrap();
 
+    let data1: Vec<u8>= createVectorDataforTest(inputDataLen);
+
     let mut wal = WriteAheadLog::new(dir.as_str()).unwrap();
-    let data: Vec<u8>=vec![1,2,3,4];
-    wal.append_entry(data);
+    wal.append_entry(data1);
     wal.flush();
+
     let file_metadata = metadata(&wal.path());
-    assert_eq!(file_metadata.unwrap().len() as u16,WriteAheadLog::block_size());
+
+    let expected_wal_size = WriteAheadLog::block_size() as u64 * expected_wal_blocks as u64;
+    assert_eq!(file_metadata.unwrap().len(), expected_wal_size as u64);
+
     let file = OpenOptions::new().read(true).open(&wal.path()).unwrap();
     let mut reader = BufReader::new(file);
     let mut log_reader=RecordEntryIterator::new(wal.path().clone()).unwrap();
     let opt_entry=log_reader.next();
     assert!(opt_entry.is_some());
     let data_read=opt_entry.unwrap();
-    let data: Vec<u8>=vec![1,2,3,4];
+    let mut data: Vec<u8>= createVectorDataforTest(inputDataLen);
     assert_eq!(data_read,data);
+    println!("before getting none");
     let opt_entry=log_reader.next();
     assert!(opt_entry.is_none());
+}
+
+
+#[test]
+fn testWriteAndReadOneRecord() {
+    testReadAndWriteSingleBlock(4 as u32, 1);
 }
 
 #[test]
@@ -74,192 +96,31 @@ fn testWriteAndReadTwoRecord() {
 
 #[test]
 fn testWriteAndReadOneRecordSpanningTwoBlocks() {
-
-    let mut dir = env::temp_dir();
-    let mut rng = rand::thread_rng();
-    let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
-    println!("dir={}",dir);
-    create_dir(&dir).unwrap();
-
-    let mut wal = WriteAheadLog::new(dir.as_str()).unwrap();
-    let mut array: [u8; WriteAheadLog::block_size() as usize] = [0; WriteAheadLog::block_size() as usize];
-    array[0]=1;
-    array[1]=2;
-    array[3]=3;
-    array[4]=4;
-
-    let data1: Vec<u8>=Vec::from(array);
-    wal.append_entry(data1);
-    wal.flush();
-
-    let file_metadata = metadata(&wal.path());
-    assert_eq!(file_metadata.unwrap().len(),(WriteAheadLog::block_size()*2) as u64);
-    let file = OpenOptions::new().read(true).open(&wal.path()).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut log_reader=RecordEntryIterator::new(wal.path().clone()).unwrap();
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_some());
-    let data_read=opt_entry.unwrap();
-    let mut array: [u8; WriteAheadLog::block_size() as usize] = [0; WriteAheadLog::block_size() as usize];
-    array[0]=1;
-    array[1]=2;
-    array[3]=3;
-    array[4]=4;
-    let data: Vec<u8>=Vec::from(array);
-    assert_eq!(data_read,data);
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_none());
+    testReadAndWriteSingleBlock(WriteAheadLog::block_size() as u32, 2);
 }
 
 #[test]
 fn testWriteAndReadOneRecordFillingOneBlock() {
-
-    let mut dir = env::temp_dir();
-    let mut rng = rand::thread_rng();
-    let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
-    println!("dir={}",dir);
-    create_dir(&dir).unwrap();
-
-    let mut wal = WriteAheadLog::new(dir.as_str()).unwrap();
-    let mut array: [u8; WriteAheadLog::block_size() as usize-11 as usize] = [0; WriteAheadLog::block_size() as usize-11  as usize];
-    array[0]=1;
-    array[1]=2;
-    array[3]=3;
-    array[4]=4;
-
-    let data1: Vec<u8>=Vec::from(array);
-    wal.append_entry(data1);
-    wal.flush();
-
-    let file_metadata = metadata(&wal.path());
-    assert_eq!(file_metadata.unwrap().len(),WriteAheadLog::block_size() as u64);
-    let file = OpenOptions::new().read(true).open(&wal.path()).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut log_reader=RecordEntryIterator::new(wal.path().clone()).unwrap();
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_some());
-    let data_read=opt_entry.unwrap();
-    let mut array: [u8; WriteAheadLog::block_size() as usize-11  as usize] = [0; WriteAheadLog::block_size() as usize-11  as usize];
-    array[0]=1;
-    array[1]=2;
-    array[3]=3;
-    array[4]=4;
-    let data: Vec<u8>=Vec::from(array);
-    assert_eq!(data_read,data);
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_none());
+    testReadAndWriteSingleBlock(WriteAheadLog::block_size() as u32-11 as u32, 1);
 }
 
 #[test]
 fn testWriteAndReadOneRecordOverflowingOneBlock() {
-
-    let mut dir = env::temp_dir();
-    let mut rng = rand::thread_rng();
-    let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
-    println!("dir={}",dir);
-    create_dir(&dir).unwrap();
-
-    let mut wal = WriteAheadLog::new(dir.as_str()).unwrap();
-    let mut array: [u8; WriteAheadLog::block_size() as usize-10 as usize] = [0; WriteAheadLog::block_size() as usize-10  as usize];
-    array[0]=1;
-    array[1]=2;
-    array[3]=3;
-    array[4]=4;
-
-    let data1: Vec<u8>=Vec::from(array);
-    wal.append_entry(data1);
-    wal.flush();
-
-    let file_metadata = metadata(&wal.path());
-    assert_eq!(file_metadata.unwrap().len(),(WriteAheadLog::block_size() as usize*2 as usize) as u64);
-    let file = OpenOptions::new().read(true).open(&wal.path()).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut log_reader=RecordEntryIterator::new(wal.path().clone()).unwrap();
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_some());
-    let data_read=opt_entry.unwrap();
-    let mut array: [u8; WriteAheadLog::block_size() as usize-10  as usize] = [0; WriteAheadLog::block_size() as usize-10  as usize];
-    array[0]=1;
-    array[1]=2;
-    array[3]=3;
-    array[4]=4;
-    let data: Vec<u8>=Vec::from(array);
-    assert_eq!(data_read,data);
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_none());
+    testReadAndWriteSingleBlock(WriteAheadLog::block_size() as u32-10 as u32, 2);
 }
 
 #[test]
 fn testWriteAndReadOneRecordSpanningThreeBlocks() {
-
-    let mut dir = env::temp_dir();
-    let mut rng = rand::thread_rng();
-    let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
-    println!("dir={}",dir);
-    create_dir(&dir).unwrap();
-
-    let mut wal = WriteAheadLog::new(dir.as_str()).unwrap();
-    let mut data: Vec<u8>=vec![1,2,3,4];
-    for i in 0..(WriteAheadLog::block_size()as u32*2 as u32) as u32 {
-        data.push(0);
-    }
-    wal.append_entry(data);
-    wal.flush();
-
-    let file_metadata = metadata(&wal.path());
-    assert_eq!(file_metadata.unwrap().len(),(WriteAheadLog::block_size() as u32*3 as u32) as u64);
-    let file = OpenOptions::new().read(true).open(&wal.path()).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut log_reader=RecordEntryIterator::new(wal.path().clone()).unwrap();
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_some());
-    let data_read=opt_entry.unwrap();
-    let mut data: Vec<u8>=vec![1,2,3,4];
-    for i in 0..(WriteAheadLog::block_size()as u32*2 as u32) as u32 {
-        data.push(0);
-    }
-    assert_eq!(data_read,data);
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_none());
+    testReadAndWriteSingleBlock(WriteAheadLog::block_size() as u32*2 as u32, 3);
 }
 
 #[test]
 fn testWriteAndReadOneRecordSpanningFourBlocks() {
-
-    let mut dir = env::temp_dir();
-    let mut rng = rand::thread_rng();
-    let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
-    println!("dir={}",dir);
-    create_dir(&dir).unwrap();
-
-    let mut wal = WriteAheadLog::new(dir.as_str()).unwrap();
-    let mut data: Vec<u8>=vec![1,2,3,4];
-    for i in 0..(WriteAheadLog::block_size()as u32*3 as u32) as u32 {
-        data.push(0);
-    }
-    wal.append_entry(data);
-    wal.flush();
-
-    let file_metadata = metadata(&wal.path());
-    assert_eq!(file_metadata.unwrap().len(),(WriteAheadLog::block_size() as u32*4 as u32) as u64);
-    let file = OpenOptions::new().read(true).open(&wal.path()).unwrap();
-    let mut reader = BufReader::new(file);
-    let mut log_reader=RecordEntryIterator::new(wal.path().clone()).unwrap();
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_some());
-    let data_read=opt_entry.unwrap();
-    let mut data: Vec<u8>=vec![1,2,3,4];
-    for i in 0..(WriteAheadLog::block_size()as u32*3 as u32) as u32 {
-        data.push(0);
-    }
-    assert_eq!(data_read,data);
-    let opt_entry=log_reader.next();
-    assert!(opt_entry.is_none());
+    testReadAndWriteSingleBlock(WriteAheadLog::block_size() as u32*3 as u32, 4);
 }
 
 #[test]
 fn testWriteAndReadTwoRecordsWithOnlyHeaderPart() {
-
     let mut dir = env::temp_dir();
     let mut rng = rand::thread_rng();
     let dir = format!("{}{}",dir.display(), rng.gen::<u32>());
